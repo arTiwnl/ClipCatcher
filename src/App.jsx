fa8cb6c9318e5ceeec06e8fdaf72cc4559b9ce51
Arthur Carvalho <contatocarvalhoarthur@gmail.com>
2024-03-08 03:03:21 -0300

    ClipCatcher

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+
+function App() {
+
+
+
+  return (
+    <>
+    
+    <h1>ClipCatcher</h1>
+    <input class="URL-box" placeholder="Cole o link do seu vídeo aqui ☺"></input>
+
+    <select class='opt'>
+		    <option value='mp3'>mp3</option>
+		    <option value='mp4'>mp4</option>
+	  </select>
+
+    <button id='btn'class="download-button">Baixar</button>
+
+    
+
+    </>
+  )
+}
+
+export default App
